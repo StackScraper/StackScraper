256a5dfafd171a75faa346dfa18d4d5e41e344e9
michalshy <91937056+michalshy@users.noreply.github.com>
2024-04-10 19:47:20 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "GUI/test.cpp"
+
+int main()
+{
+    std::string msg = "Hello world";
+
+    std::cout<<msg<<" "<<test<<std::endl;
+
+    return 0;
+}
