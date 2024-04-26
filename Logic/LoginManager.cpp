d03a3822948d98b2b0775052c818751d6dd79d21
michalshy <91937056+michalshy@users.noreply.github.com>
2024-04-26 17:35:24 +0200

    Small updates in states and helping vars declarations

---
+++ Logic/LoginManager.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Michin on 26.04.2024.
+//
+
+#include "LoginManager.hpp"
+
+bool LoginManager::login(std::string& log, std::string& pass) {
+    //todo
+    if(log == "admin" && pass == "admin")
+    {
+        return true;
+    }
+    return false;
+}
+
+bool LoginManager::registerNew(std::string& log, std::string& email, std::string& pass) {
+    //todo
+    return false;
+}
--- States/StateIdle.cpp
+++ States/StateIdle.cpp
@@ -5,29 +5,25 @@
 #include "StateIdle.hpp"
 #include "../Texts/AllTexts.hpp"
 #include "../Logic/TextFormatter.hpp"
-using namespace TextFunctions;
-using namespace TextColors;
 
 void StateIdle::onEnter(){
     State<States>::onEnter();
     system("cls");
-    changeTextColor(BEIGE);
-    print(IdleTexts::title);
+    TextFunctions::changeTextColor(TextColors::BEIGE);
+    TextFunctions::print(IdleTexts::title);
 }
 
 void StateIdle::onUpdate() {
     State<States>::onUpdate();
-    setCursor(40, 7);
-    typeWriteMessage(IdleTexts::helloIns, 50);
+    TextFunctions::setCursor(40, 7);
+    TextFunctions::typeWriteMessage(IdleTexts::helloIns, 50);
     prompt->getPrompt();
     if(prompt->retValues() == "login")
     {
-        printf("login switch");
         mFsm.setCurrentState(States::LOGIN);
     }
     else if(prompt->retValues() == "register")
     {
-        printf("register switch");
         mFsm.setCurrentState(States::REGISTER);
     }
     else
