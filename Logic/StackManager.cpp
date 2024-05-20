b197b1a4c24275a4a964e4ccadd7c7b650a924e0
Kromczetto <jk302106@student.polsl.pl>
2024-05-20 17:19:35 +0200

    fixing, fixing, fixing ...

--- Logic/StackManager.cpp
+++ Logic/StackManager.cpp
@@ -8,9 +8,7 @@
 #include <nlohmann/json.hpp>
 #include <string>
 
-//https://api.stackexchange.com/2.3/questions/123/answers?pagesize=3&order=desc&sort=votes&site=stackoverflow&filter=withbody
-//search: https://api.stackexchange.com/2.3/similar?order=desc&sort=activity&title=how%20to%20declarate%20array%20of%20pointer&site=stackoverflow&filter=withbody
-
+//SEARCH: https://api.stackexchange.com/2.3/search/advanced?order=desc&sort=relevance&q=how%20to%20declare%20array%20of%20string%20in%20cpp&site=stackoverflow&filter=withbody
 std::string StackManager::askQuestion() {
     cpr::Response r = cpr::Get(cpr::Url{finalInput});
     return r.text;
@@ -18,7 +16,7 @@ std::string StackManager::askQuestion() {
 }
 void StackManager::setQuestion(std::string newInput) {
     questionInput = regex_replace(newInput, std::regex(" "), space);
-    finalInput = baseInput+apiVesion+"similar?order=desc&sort=activity&title="+questionInput+"&site=stackoverflow&filter=withbody";
+    finalInput = baseInput+apiVesion+"search/advanced?order=desc&sort=relevance&q="+questionInput+"&site=stackoverflow&filter=withbody";
 }
 void StackManager::setQuestionByTags(std::string newInput) {
     questionInput = regex_replace(newInput, std::regex(" "), space);
@@ -125,6 +123,11 @@ std::string StackManager::ReturnNiceCode(std::string input) {
     while (pos != std::string::npos) {
         input.replace(pos, 6, "\"");
         pos = input.find("&quot;", pos + 1);
+    }
+    pos = input.find("&amp;");
+    while (pos != std::string::npos) {
+        input.replace(pos, 5, "&");
+        pos = input.find("&amp;", pos + 1);
     }
     return input;
 }
