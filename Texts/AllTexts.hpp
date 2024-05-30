adba0368110880101f0db1e7caa6778c9e67ba89
michalshy <91937056+michalshy@users.noreply.github.com>
2024-05-30 13:36:49 +0200

    Add --help and -h switch

--- Texts/AllTexts.hpp
+++ Texts/AllTexts.hpp
@@ -199,5 +199,64 @@ namespace ListState
     static std::string returnText = "Type return to get back to menu";
 }
 
+///namespace for manual
+namespace Manual
+{
+    static std::string manual = "(01010011 01110100 01100001 01100011 01101011 01010011 01100011 01110010 01100001 01110000 01100101 01110010)\n"
+                                "Manual of StackScraper \n"
+                                "\n"
+                                "Provided states and commands which can be used in them\n"
+                                "Idle:\n"
+                                " - login - go to login\n"
+                                " - register - go to register\n"
+                                "in both of login and register app will ask about credentials.\n"
+                                "Login approval will result in transfering to main menu\n"
+                                "Register approval will result in transfering to login\n"
+                                "\n"
+                                "Main menu:\n"
+                                " - question - ask question on stack overflos\n"
+                                " - tags - search for questions by tags\n"
+                                " - history - check your history\n"
+                                " - favourites - check your favourites questions\n"
+                                "\n"
+                                "Question:\n"
+                                "prompt your question to ask and go to result,\n"
+                                "type return to go back to menu\n"
+                                "\n"
+                                "Tags:\n"
+                                "prompt tags to check for questions and move to list of them,\n"
+                                "type return to go back to menu\n"
+                                "\n"
+                                "List of questions:\n"
+                                "choose question from list by prompting number,\n"
+                                "type return to go back to menu\n"
+                                "\n"
+                                "History:\n"
+                                "lists your recent questions,\n"
+                                "type return to go back,\n"
+                                "type number of question to move to this question,\n"
+                                "type f$ where $ is number of question to add it to favourites\n"
+                                "\n"
+                                "Favourites:\n"
+                                "lists your favourites questions,\n"
+                                "type return to go back,\n"
+                                "type number of question to move to this question,\n"
+                                "type d$ where $ is number of question to delete it from favourites\n"
+                                "\n"
+                                "Result:\n"
+                                "you can move to result from different states,\n"
+                                "from question by prompting question,\n"
+                                "from list of tags, history or favourites by choosing number from list\n"
+                                "it always gives back question with max 3 top rated answers\n"
+                                "and possibility to type return to go back to previous state\n"
+                                "\n"
+                                "MOST OF THE STATES HAVE AUTOCOMPLETE SO YOU CAN TYPE\n"
+                                "R INSTEAD OF RETURN TO EXECUTE DESIRED COMMAND";
+
+    static std::string exit = "\nPress any key + enter to exit\n";
+    static std::string help = "\nYou can also double check commands in the new file HELP.txt which just got created"
+                              "in the exe directory\n";
+}
+
 
 #endif //INC_2024__TAB_DSA__8_BRODZIAK_ALLTEXTS_HPP
