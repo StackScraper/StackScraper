c2d6f37705fe3ed3c77e6428c16988e85e1b83a7
michalshy <91937056+michalshy@users.noreply.github.com>
2024-07-14 17:59:25 +0200

    final implemetation of lib structures

---
+++ Third-Party/SyntXReadMe.txt
@@ -0,0 +1,3 @@
+git clone https://github.com/michalshy/SyntX
+
+and then follow SyntX readme
--- Third-Party/SyntaxLib/SyntX/SyntX.cpp
+++
@@ -1,7 +0,0 @@
-#include "SyntX.hpp"
-
-__attribute__((visibility("default")))
-void CLogger::Log(const char* msg)
-{
-    printf("Logged: %s", msg);
-}
